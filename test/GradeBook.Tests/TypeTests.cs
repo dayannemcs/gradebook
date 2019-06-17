e6e1432c010709dedc90a817c61689f12c5b812c
Dayanne Souza <dayanne.souza@farfetch.com>
2019-06-17 09:13:29 +0100

    New tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,46 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CsharpCanPassByReference()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New name");
+
+            Assert.Equal("Book 1", book1.Name);
+           
+        }
+        
+        [Fact]
+        public void CsharpisPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New name");
+
+            Assert.Equal("Book 1", book1.Name);
+           
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book  = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New name");
+
+            Assert.Equal("New name", book1.Name);
+           
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
